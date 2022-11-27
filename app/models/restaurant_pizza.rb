5f4a0f27eac738f7a949c720538223483e524ef7
Patrick-Njiru <patrick.njiru@student.moringaschool.com>
2022-11-27 21:30:17 +0300

    Add validation.

--- app/models/restaurant_pizza.rb
+++ app/models/restaurant_pizza.rb
@@ -1,4 +1,5 @@
 class RestaurantPizza < ApplicationRecord
     belongs_to :pizza
     belongs_to :restaurant
+    validates :price, inclusion: {in: [1..30], message: "Price must be between 1 and 30."}
 end
