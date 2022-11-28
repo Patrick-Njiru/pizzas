67dfd107a66d61cdbdc7adaf7c1c56e5b4b21ce3
Patrick-Njiru <patrick.njiru@student.moringaschool.com>
2022-11-28 15:32:01 +0300

    Restructure code.

--- app/controllers/restaurant_pizzas_controller.rb
+++ app/controllers/restaurant_pizzas_controller.rb
@@ -1,12 +1,20 @@
 class RestaurantPizzasController < ApplicationController
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
     wrap_parameters format: []
     
     # POST /restaurantpizzas
     def create
-        restaurant_pizza = RestaurantPizza.create!(price: params[:price], restaurant_id: params[:restaurant_id], pizza_id: params[:pizza_id])
+        restaurant_pizza = RestaurantPizza.create!(restaurant_pizza_params)
         render json: restaurant_pizza.pizza, status: :created
-        
-    rescue ActiveRecord::RecordInvalid => invalid
+    end
+
+    private
+
+    def restaurant_pizza_params
+        params.permit(:price, :restaurant_id, :pizza_id)
+    end
+
+    def render_unprocessable_entity_response(invalid)
         render json: {errors: invalid.record.errors.full_messages}, status: :unprocessable_entity
     end
 end
