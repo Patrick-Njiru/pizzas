f70f30e58baaa756044430fef67664a865c21f30
Patrick-Njiru <patrick.njiru@student.moringaschool.com>
2022-11-28 11:29:27 +0300

    Add methods.

--- app/controllers/restaurant_pizzas_controller.rb
+++ app/controllers/restaurant_pizzas_controller.rb
@@ -1,2 +1,14 @@
 class RestaurantPizzasController < ApplicationController
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+
+    def create
+        restaurant_pizza = RestaurantPizza.create!(price: params[:price], user_id: params[:user_id], pizza_id: params[:pizza_id])
+        render json: restaurant_pizza, status: :created
+    end
+    
+    private 
+
+    def render_unprocessable_entity_response(invalid)
+        render json: {errors: invalid.record.errors.full_messages}, status: :unprocessable_entity
+    end
 end
