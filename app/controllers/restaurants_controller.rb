468d062f8991596aa67f130bb3ba60fb2c7eeee3
Patrick-Njiru <patrick.njiru@student.moringaschool.com>
2022-11-28 13:08:30 +0300

    Add methods.

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,35 @@
 class RestaurantsController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+    def index
+
+        render json: Restaurant.all
+    end
+
+    def show
+        restaurant = find_restaurant
+        render json: restaurant, 
+        # include: ['restaurant.pizzas']
+    end
+
+    def destroy
+        restaurant = find_restaurant
+        restaurant.destroy
+        head :no_content
+    end
+
+    private
+
+    def restaurant_params
+        params.permit(:id, :name, :address)
+    end
+
+    def find_restaurant
+        Restaurant.find(params[:id])
+    end
+
+    def render_not_found_response
+        render json: {error: "Restaurant not found"}, status: :not_found
+    end
 end
+
