0f5737386f3d610ef795ac06f506bf996509e27a
Patrick-Njiru <patrick.njiru@student.moringaschool.com>
2022-11-28 13:07:20 +0300

    Uncomment code.

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@ puts "Seeding data..."
 
 Restaurant.destroy_all
 Pizza.destroy_all
-# RestaurantPizza.destroy_all
+RestaurantPizza.destroy_all
 
 5.times{ Restaurant.create!(name: Faker::Restaurant.unique.name, address: Faker::Address.full_address)}
 
@@ -13,14 +13,14 @@ Pizza.create!([
     {name: 'Margherita', ingredients: "basil, fresh mozzarella, tomatoes"}
 ])
 
-# RestaurantPizza.create([
-#     {price: 5, pizza_id: 1, restaurant_id: 3},
-#     {price: 20, pizza_id: 3, restaurant_id: 1},
-#     {price: 10, pizza_id: 2, restaurant_id: 5},
-#     {price: 14, pizza_id: 4, restaurant_id: 1},
-#     {price: 14, pizza_id: 1, restaurant_id: 1},
-#     {price: 14, pizza_id: 3, restaurant_id: 4},
-#     {price: 14, pizza_id: 4, restaurant_id: 2}
-# ])
+RestaurantPizza.create([
+    {price: 5, pizza_id: 1, restaurant_id: 3},
+    {price: 20, pizza_id: 3, restaurant_id: 1},
+    {price: 10, pizza_id: 2, restaurant_id: 5},
+    {price: 14, pizza_id: 4, restaurant_id: 1},
+    {price: 14, pizza_id: 1, restaurant_id: 1},
+    {price: 14, pizza_id: 3, restaurant_id: 4},
+    {price: 14, pizza_id: 4, restaurant_id: 2}
+])
 
 puts "Seeding completed"
