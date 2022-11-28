4b5356520743b39a79548f540e029c49dc807210
Patrick-Njiru <patrick.njiru@student.moringaschool.com>
2022-11-28 09:31:29 +0300

    Modify file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,7 @@ puts "Seeding data..."
 
 Restaurant.destroy_all
 Pizza.destroy_all
+# RestaurantPizza.destroy_all
 
 5.times{ Restaurant.create!(name: Faker::Restaurant.unique.name, address: Faker::Address.full_address)}
 
@@ -11,5 +12,15 @@ Pizza.create!([
     {name: 'Veggie', ingredients: "pepper, mushroom, eggplant, onion"},
     {name: 'Margherita', ingredients: "basil, fresh mozzarella, tomatoes"}
 ])
+
+# RestaurantPizza.create([
+#     {price: 5, pizza_id: 1, restaurant_id: 3},
+#     {price: 20, pizza_id: 3, restaurant_id: 1},
+#     {price: 10, pizza_id: 2, restaurant_id: 5},
+#     {price: 14, pizza_id: 4, restaurant_id: 1},
+#     {price: 14, pizza_id: 1, restaurant_id: 1},
+#     {price: 14, pizza_id: 3, restaurant_id: 4},
+#     {price: 14, pizza_id: 4, restaurant_id: 2}
+# ])
 
 puts "Seeding completed"
