d92a89020fa3fe0c2852022085940a28e80a3421
Patrick-Njiru <patrick.njiru@student.moringaschool.com>
2022-11-28 09:09:08 +0300

    Update schema.

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,4 @@
-ActiveRecord::Schema[7.0].define(version: 2022_11_27_163032) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_28_055558) do
   create_table "pizzas", force: :cascade do |t|
     t.string "name"
     t.string "ingredients"
@@ -8,10 +8,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_27_163032) do
 
   create_table "restaurant_pizzas", force: :cascade do |t|
     t.integer "price"
-    t.integer "pizza_id"
-    t.integer "restaurant_id"
+    t.integer "restaurant_id", null: false
+    t.integer "pizza_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["pizza_id"], name: "index_restaurant_pizzas_on_pizza_id"
+    t.index ["restaurant_id"], name: "index_restaurant_pizzas_on_restaurant_id"
   end
 
   create_table "restaurants", force: :cascade do |t|
@@ -21,4 +23,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_27_163032) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "restaurant_pizzas", "pizzas"
+  add_foreign_key "restaurant_pizzas", "restaurants"
 end
