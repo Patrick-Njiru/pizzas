72608cebe7801e14b05a76f286d33eae017708bd
Patrick-Njiru <patrick.njiru@student.moringaschool.com>
2022-11-28 13:04:41 +0300

    Modify file.

--- app/controllers/restaurant_pizzas_controller.rb
+++ app/controllers/restaurant_pizzas_controller.rb
@@ -2,7 +2,7 @@ class RestaurantPizzasController < ApplicationController
 
     def create
         restaurant_pizza = RestaurantPizza.create!(price: params[:price], restaurant_id: params[:restaurant_id], pizza_id: params[:pizza_id])
-        render json: restaurant_pizza, status: :created
+        render json: restaurant_pizza.pizza, status: :created
     rescue ActiveRecord::RecordInvalid => invalid
         render json: {errors: invalid.record.errors.full_messages}, status: :unprocessable_entity
     end
