6ac076a6c2387fbd6450dbca98d26f138cbc88e4
Patrick-Njiru <patrick.njiru@student.moringaschool.com>
2022-11-29 19:37:50 +0300

    Modify file.

--- app/models/restaurant_pizza.rb
+++ app/models/restaurant_pizza.rb
@@ -1,5 +1,5 @@
 class RestaurantPizza < ApplicationRecord
     belongs_to :pizza
     belongs_to :restaurant
-    validates :price, comparison: { greater_than: 0, less_than: 31}
+    validates :price, comparison: { greater_than_or_equal_to: 1, less_than_or_equal_to: 30}
 end
